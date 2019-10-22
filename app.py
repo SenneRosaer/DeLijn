f5b67f2b7d6a2547e04a392679b743364af299e8
SenneRosaer <s0172200@ad.ua.ac.be>
2019-10-22 13:53:00 +0200

    idk

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask
 import json
-from flask import render_template
+from flask import render_template,jsonify
 from flask_restful import Resource,Api
 
 app = Flask(__name__)
@@ -40,21 +40,76 @@ class HelloWorld(Resource):
             print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
 
-class Test(Resource):
+class Haltes(Resource):
+    def get(self,province, line, richting):
+        try:
+            conn = http.client.HTTPSConnection('api.delijn.be')
+            conn.request("GET", "/DLKernOpenData/v1/beta/lijnen/"+province+"/"+line+"/lijnrichtingen/"+richting+"/haltes?%s" % params, "{body}", headers)
+            response = conn.getresponse()
+            data = response.read()
+            conn.close()
+
+
+            haltes = json.loads(data)
+
+            returndata = []
+
+            for halte in haltes["haltes"]:
+                coor = halte["geoCoordinaat"]
+
+                returndata.append({"lat":coor["latitude"],"long":coor["longitude"]  })
+
+
+            return  jsonify({"haltes":returndata})
+        except Exception as e:
+            print("[Errno {0}] {1}".format(e.errno, e.strerror))
+
+class Route(Resource):
     def get(self,province, line, richting):
         try:
             conn = http.client.HTTPSConnection('api.delijn.be')
             conn.request("GET", "/DLKernOpenData/v1/beta/lijnen/"+province+"/"+line+"/lijnrichtingen/"+richting+"/dienstregelingen?%s" % params, "{body}", headers)
             response = conn.getresponse()
             data = response.read()
-            print(data)
             conn.close()
-            return json.loads(data)
+
+
+            regeling = json.loads(data)
+            temp = regeling["ritDoorkomsten"][0]
+            temp2 = temp["doorkomsten"]
+            returndata = []
+            haltes = []
+
+            for item in temp2:
+                haltes.append(item["haltenummer"])
+
+            conn = http.client.HTTPSConnection('api.delijn.be')
+            conn.request("GET",
+                         "/DLKernOpenData/v1/beta/lijnen/" + province + "/" + line + "/lijnrichtingen/" + richting + "/haltes?%s" % params,
+                         "{body}", headers)
+            response = conn.getresponse()
+            data = response.read()
+            conn.close()
+
+            haltesCor = json.loads(data)
+
+            for firstHalte in haltes:
+                for secondHalte in haltesCor["haltes"]:
+                    if firstHalte == secondHalte["haltenummer"]:
+                        returndata.append({"lat": secondHalte["geoCoordinaat"]["latitude"], "long": secondHalte["geoCoordinaat"]["longitude"]})
+
+
+            return  jsonify({"haltes":returndata})
         except Exception as e:
             print("[Errno {0}] {1}".format(e.errno, e.strerror))
 
+
+
+
 api.add_resource(HelloWorld, '/')
-api.add_resource(Test,'/<province>/<line>/<richting>')
+api.add_resource(Haltes,'/haltes/<province>/<line>/<richting>')
+api.add_resource(Route,'/route/<province>/<line>/<richting>')
+
 
 
 
